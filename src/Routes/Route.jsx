a64473f8fa5003ceb210911cbbd7ecbf0cc805ec
rayhanbri <mohammadrayhanx0@gmail.com>
2025-04-18 22:58:17 +0600

    folder setup

--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -4,6 +4,8 @@ import React from 'react';
 import {
   createBrowserRouter,
 } from "react-router";
+import Root from '../Pages/Root/Root';
+import ErrorPage from '../Pages/ErrorPage/ErrorPage';
   
 
 // router ta k export kore dhibho
@@ -12,6 +14,7 @@ import {
  export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World fsdsdaf</div>,
+    Component:Root,
+    errorElement: <ErrorPage></ErrorPage>
   },
 ]);

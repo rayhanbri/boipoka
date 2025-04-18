29362732e2aec719a0972ff7a5a05523d965519d
rayhanbri <mohammadrayhanx0@gmail.com>
2025-04-19 02:45:36 +0600

    data load with use effect

--- src/Pages/Books/Books.jsx
+++ src/Pages/Books/Books.jsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Books = () => {
+  const [allbooks,setAllBooks] =useState();
+
+  useEffect(()=>{
+    fetch('booksData.json')
+    .then(res => res.json())
+    .then(data => console.log(data))
+  },[])
   return (
     <div>
       <h1>hello ami books</h1>

3023cecfaf5df3cdb7858c77771996c62d31f253
rayhanbri <mohammadrayhanx0@gmail.com>
2025-04-19 16:56:07 +0600

    book card done

--- src/Pages/Book/Book.jsx
+++ src/Pages/Book/Book.jsx
@@ -1,12 +1,12 @@
 // import React, { use } from 'react';
-import { FcRating } from "react-icons/fc";
+import { FaStarHalfAlt } from "react-icons/fa";
 const Book = ({singleBook}) => {
   console.log(singleBook)
  
   // const  data = use(booksPromise)
 
   // console.log(data)
-   const {bookName,author,image,rating,category} =singleBook;
+   const {bookName,author,image,rating,category,totalPages,tags} =singleBook;
 
   return (
     <div className="card bg-base-100 w-96 shadow-sm border border-lg p-6">
@@ -16,15 +16,21 @@ const Book = ({singleBook}) => {
       src={image}
       alt="Shoes" />
   </figure>
-  <div className="card-body">
+  <div className="card-body ">
+    <div className="flex justify-start gap:20px ">
+       {
+         tags.map(tag =><p className="text-green-400 "><smaill className="bg-gray-200 w-9 p-2 rounded-full">{tag}</smaill></p>)
+       }
+    </div>
     <h2 className="card-title">
-      Card Title
-      <div className="badge badge-secondary">NEW</div>
+     {bookName}
+      <div className="badge badge-secondary">{totalPages}</div>
     </h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <p className="font-semibold text-sm">Book By:{author}</p>
+    <div className="border-1 border-dashed border-gray-300 "></div>
     <div className="card-actions justify-end">
       <div className="badge badge-outline">{category}</div>
-      <div className="badge badge-outline">{rating}<FcRating /></div>
+      <div className="badge badge-outline">{rating}<FaStarHalfAlt /></div>
     </div>
   </div>
 </div>

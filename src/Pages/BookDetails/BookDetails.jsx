46ce8a0d003c48ead76016cf1355a916c4613620
rayhanbri <mohammadrayhanx0@gmail.com>
2025-04-20 20:15:47 +0600

    bookdetails added and its data

--- src/Pages/BookDetails/BookDetails.jsx
+++ src/Pages/BookDetails/BookDetails.jsx
@@ -1,21 +1,31 @@
-import React, { useState } from 'react';
+
 import { useLoaderData, useParams } from 'react-router';
 
 const BookDetails = () => {
-  const {id}  = useParams();
-   
-  const data = useLoaderData() || []
- const [bookData,setBookData] = useState(data || []);
-  const singleBook = bookData.find(book => book.bookId == id)
-  console.log(singleBook)
+  const{ id} = useParams();
+  // console.log(id)
+  const bookId = parseInt(id)
+  const data = useLoaderData();
+  // console.log(data)
+  const singleData =data.find(book => book.bookId === bookId)
+
+  // console.log(singleData);
+
+  const {bookName,image} = singleData;
 
   // ei console home e giye abar click korte hoi 
 
   // console.log(data)
 
   return (
-    <div>
-      <h1>Book details</h1>
+    <div className='w-2/3 mx-auto m-10'>
+     <img className="w-36 mx-auto " src={image} alt="" />
+     <h1 className='text-4xl font-bold  text-center'>{bookName}</h1>
+     <div className='text-center gap-5 m-5'>
+     <button className="btn btn-warning">Wishlist</button>
+     <button className="btn btn-error ml-5">Read</button>
+     </div>
+     <div className='divider'></div>
     </div>
   );
 };
